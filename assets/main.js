6792e8e20da11354f23f7991a29ab339cf0dee50
tamburkey <petem14@gmail.com>
2020-05-06 19:00:55 -0400

    add solution

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,47 @@
-const wordToGuessElement = document.querySelector('#word-to-guess')
-const currentWord = wordToGuessElement.innerText
+const currentWordElement = document.querySelector('#word-to-guess')
 
-// const letterInputElement = document.querySelector('#letter-input')
-// const letterSubmitButton = document.querySelector('#letter-submit-button')
-// const lettersAttemptedElement = document.querySelector('#letters-attempted')
+const guessElement = document.querySelector('#letter-input')
+const submitButton = document.querySelector('#letter-submit-button')
+const  guessedElement = document.querySelector('#letters-attempted')
+
+let guessCountElement = document.querySelector("#guess-count")
+let guessesRemaining = 5
+
+let finalWord = currentWordElement.innerHTML
+let hiddenChar = "-"
+
+currentWordElement.innerHTML = hiddenChar.repeat(finalWord.length)
+
+submitButton.addEventListener("click", function () {
+    let guessedLetter = guessElement.value
+    if(!guessedElement.innerHTML.includes(guessedLetter)){
+        guessedElement.innerHTML += guessedLetter
+    } else {
+        alert("letter already guessed")
+    }
+    if(finalWord.includes(guessedLetter)){
+        // update currentword element
+        let prevWord = currentWordElement.innerHTML
+        let finalString = ""
+
+        let count = 0
+        while(count < finalWord.length){
+            if(prevWord[count]===hiddenChar && finalWord[count]===guessedLetter){
+                finalString+=guessedLetter
+            } else if (prevWord[count]===hiddenChar){
+                finalString+=hiddenChar
+            } else {
+                finalString+=prevWord[count]
+            }
+            count++
+        }
+        currentWordElement.innerHTML = finalString
+    } else {
+        guessesRemaining--
+        guessCountElement.innerHTML = "guesses left:"+guessesRemaining
+    }
+    if(guessesRemaining===0){
+        alert("lose")
+        submitButton.disabled = true
+    }
+})
